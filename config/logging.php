364e0fd424b1b42f7fdaca1eedd76d6b1181f84c
realzuanzuan <shaozuanzuan@gmail.com>
2025-07-13 12:14:59 +0700

    update stripe

--- config/logging.php
+++ config/logging.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => 'single',
+    'default' => 'stack',
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['daily', 'mysql'], // 把 mysql 也加入进来
             'ignore_exceptions' => false,
         ],
 
