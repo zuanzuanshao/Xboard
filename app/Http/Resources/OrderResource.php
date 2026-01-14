e761146f2c3b7819bf29bd79a76afc9f44fa16a6
realzuanzuan <shaozuanzuan@gmail.com>
2026-01-14 15:54:02 +0800

    过滤支付敏感信息

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -19,8 +19,22 @@ class OrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $data = parent::toArray($request);
+
+        // Filter sensitive payment data - only keep safe fields for display
+        if (isset($data['payment']) && is_array($data['payment'])) {
+            $data['payment'] = [
+                'id' => $data['payment']['id'] ?? null,
+                'name' => $data['payment']['name'] ?? null,
+                'payment' => $data['payment']['payment'] ?? null,
+                'icon' => $data['payment']['icon'] ?? null,
+                'handling_fee_fixed' => $data['payment']['handling_fee_fixed'] ?? null,
+                'handling_fee_percent' => $data['payment']['handling_fee_percent'] ?? null,
+            ];
+        }
+
         return [
-            ...parent::toArray($request),
+            ...$data,
             'period' => PlanService::getLegacyPeriod((string)$this->period),
             'plan' => $this->whenLoaded('plan', fn() => PlanResource::make($this->plan)),
         ];
